f401c22c907cbdd50db1feeef8b2a71e5cbb4d6e
Manish Choudhary <me.developer4u@gmail.com>
2026-07-30 22:06:42 +0530

    Time: 1 ms (99.14%), Space: 44.3 MB (56.38%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -1,24 +1,15 @@
 class Solution {
     public String trimTrailingVowels(String s) {
-        String str="";
-        Set<Character> ch= Set.of('a','e','i','o','u');
-
-        int j=0;
-        for(int i=s.length()-1; i>=0; i--){
-            if(ch.contains(s.charAt(i))){
-                continue;
+        int i=s.length()-1;
+        while(i>=0){
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                i--;
             }
             else{
-                j=i;
                 break;
             }
         }
-        for(int k=0; k<=j; k++){
-            if(k==j && ch.contains(s.charAt(k))){
-                break;
-            }
-            str += s.charAt(k);
-        }
-        return str;
+        return s.substring(0, i+1);
     }
 }
