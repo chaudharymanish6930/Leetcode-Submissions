0a3c338159fb6d577a318e8d789bb832447c939e
Manish Choudhary <me.developer4u@gmail.com>
2026-09-26 22:41:22 +0530

    Time: 1 ms (95.7%), Space: 85.2 MB (58.35%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -6,12 +6,15 @@ class Solution {
         for(int i=1; i<nums.length; i++){
             if(nums[i]>nums[i-1]){
                 inc= false;
-            }
-            if(nums[i]<nums[i-1]){
-                dec=false;
+                break;
             }
         }
-        
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]<nums[i-1]){
+                dec=false;
+                break;
+            }
+        }
         return inc || dec;
     }
 }
